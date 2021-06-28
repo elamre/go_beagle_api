f7e52159c2f0b2d6cd0940d6e2943a17ee473389
elamre <elmarvanrijnswou@hotmail.com>
2021-06-28 15:48:50 +0200

    Revisited main to work with new project layout

--- cmd/dump-spi/main.go
+++ cmd/dump-spi/main.go
@@ -21,25 +21,25 @@ func main() {
 	timeout := uint32(5000)    // in milliseconds
 	latency := uint32(200)     // in milliseconds
 	//length := int32(0)
-	var beagle beagle.Beagle
+	var beagleDevice beagle.Beagle
 	var err error
 
-	beagle, err = beagle.NewBeagle(port)
+	beagleDevice, err = beagle.NewBeagle(port)
 	checkError(err)
-	defer func() { checkError(beagle.Close()) }()
+	defer func() { checkError(beagleDevice.Close()) }()
 
-	samplerate, err = beagle.BgSampleRate(beagle, samplerate)
+	samplerate, err = beagle.BgSampleRate(beagleDevice, samplerate)
 	checkError(err)
-	beagle.SetTimeout(timeout)
-	beagle.SetLatency(latency)
-	if beagle.IsFullSpeed() {
+	beagleDevice.SetTimeout(timeout)
+	beagleDevice.SetLatency(latency)
+	if beagleDevice.IsFullSpeed() {
 		log.Println("full speed")
 	} else {
 		log.Println("high speed")
 	}
-	beagle.SpiConfigure(beagle.BG_SPI_SS_ACTIVE_LOW, beagle.BG_SPI_SCK_SAMPLING_EDGE_RISING, beagle.BG_SPI_BITORDER_MSB)
-	beagle.TargetPower(beagle.BG_TARGET_POWER_OFF)
-	beagle.ReadToCsvLoop(beagle, beagle.BG_PROTOCOL_SPI, "Infineon", "GtpGo", "output")
+	beagleDevice.SpiConfigure(beagle.BG_SPI_SS_ACTIVE_LOW, beagle.BG_SPI_SCK_SAMPLING_EDGE_RISING, beagle.BG_SPI_BITORDER_MSB)
+	beagleDevice.TargetPower(beagle.BG_TARGET_POWER_OFF)
+	beagle.ReadToCsvLoop(beagleDevice, beagle.BG_PROTOCOL_SPI, "Infineon", "GtpGo", "output")
 	time.Sleep(10000 * time.Millisecond)
 	beagle.StopReadToCsvLoop()
 }
